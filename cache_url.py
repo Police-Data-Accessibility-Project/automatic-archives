a1d58afae944e16be22fd60cde880516c52bc2d9
Marty <mbodenator@gmail.com>
2023-11-17 14:39:17 -0500

    update urls

--- cache_url.py
+++ cache_url.py
@@ -2,6 +2,9 @@ import json
 from datetime import datetime, timedelta
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def match_freq(update_frequency):
 
@@ -24,80 +27,81 @@ def match_freq(update_frequency):
     update_delta = update_frequency_mapping.get(update_frequency)
 
     return update_delta
+    
 
-api_key = 'Bearer ' + os.getenv("PDAP_API_KEY")
-response = requests.get("https://data-sources.pdap.io/archives", headers={'Authorization': api_key})
+api_key = "Bearer " + os.getenv("VUE_APP_PDAP_API_KEY")
+response = requests.get("https://data-sources.pdap.io/api/archives", headers={"Authorization": api_key})
 data = response.json()
 
 # Extract url info and cache if needed
 exceptions = []
 if data is not str:
     for entry in data:
-        entry['broken_source_url_as_of'] = None
-        source_url = entry.get('source_url')
+        entry["broken_source_url_as_of"] = None
+        source_url = entry.get("source_url")
         if source_url is None:
-            entry['broken_source_url_as_of'] = datetime.now().strftime('%m-%d-%Y')
+            entry["broken_source_url_as_of"] = datetime.now().strftime("%m-%d-%Y")
             try:
                 entry_json = json.dumps(entry)
-                response = requests.put("https://data-sources.pdap.io/archives", json=entry_json, headers={'Authorization': api_key})
-                raise Exception('No source_url')
+                response = requests.put("https://data-sources.pdap.io/api/archives", json=entry_json, headers={"Authorization": api_key})
+                raise Exception("No source_url")
             except Exception as error:
                 print(str(error))
-                exceptions.append({'agency_name': entry.get('agency_name'),
-                                'source_url': source_url, 
-                                'exception': str(error)})
+                exceptions.append({"agency_name": entry.get("agency_name"),
+                                "source_url": source_url, 
+                                "exception": str(error)})
                 continue
-        update_delta = match_freq(entry.get('update_frequency'))
-        agency_name = entry.get('agency_name')
+        update_delta = match_freq(entry.get("update_frequency"))
+        agency_name = entry.get("agency_name")
         if update_delta is None:
             update_delta = datetime.max - datetime.today()
         else:
             update_delta = timedelta(days=int(update_delta))
 
-        last_cached = entry.get('last_cached')
+        last_cached = entry.get("last_cached")
         if last_cached is not None:
-            last_cached = datetime.strptime(last_cached, '%Y-%m-%d')
+            last_cached = datetime.strptime(last_cached, "%Y-%m-%d")
         else:
             last_cached = datetime.min
         
         # Check if website exists in archive and compare archived website to current site
         website_info_data = None
         try:
-            website_info = requests.get(f'https://archive.org/wayback/available?url={source_url}')
+            website_info = requests.get(f"https://archive.org/wayback/available?url={source_url}")
             website_info_data = website_info.json()
-            if website_info_data['archived_snapshots']:
-                website_info_data_last_cached = datetime.strptime(website_info_data['archived_snapshots']['closest']['timestamp'], "%Y%m%d%H%M%S")
-                website_info_data_source_url = website_info_data['archived_snapshots']['closest']['url']
+            if website_info_data["archived_snapshots"]:
+                website_info_data_last_cached = datetime.strptime(website_info_data["archived_snapshots"]["closest"]["timestamp"], "%Y%m%d%H%M%S")
+                website_info_data_source_url = website_info_data["archived_snapshots"]["closest"]["url"]
                 if website_info_data_last_cached > last_cached:
                     last_cached = website_info_data_last_cached
         except Exception as error:
             print(str(error))
-            website_info_data = {'archived_snapshots': None}
+            website_info_data = {"archived_snapshots": None}
 
 
         # Cache if never cached or more than update_delta days have passed since last_cache
-        if not website_info_data['archived_snapshots'] or last_cached + update_delta < datetime.today():
+        if not website_info_data["archived_snapshots"] or last_cached + update_delta < datetime.today():
             try:
                 api_url = "http://web.archive.org/save/{}".format(source_url)
                 archive = requests.post(api_url)
                 # Update the last_cached date if cache is successful
-                entry['last_cached'] = datetime.now().strftime('%m-%d-%Y')
+                entry["last_cached"] = datetime.now().strftime("%m-%d-%Y")
             except Exception as error:
                 print(str(error))
-                exceptions.append({'agency_name': entry.get('agency_name'),
-                                'source_url': source_url, 
-                                'exception': str(error)})
+                exceptions.append({"agency_name": entry.get("agency_name"),
+                                "source_url": source_url, 
+                                "exception": str(error)})
         else:
-            entry['last_cached'] = last_cached.strftime('%m-%d-%Y')
+            entry["last_cached"] = last_cached.strftime("%m-%d-%Y")
     
         # Send updated data to Data Sources
         entry_json = json.dumps(entry)
-        response = requests.put("https://data-sources.pdap.io/archives", json=entry_json, headers={'Authorization': api_key})
+        response = requests.put("https://data-sources.pdap.io/api/archives", json=entry_json, headers={"Authorization": api_key})
 
 # Write any exceptions to a daily error log
-file_name = 'ErrorLogs/' + datetime.now().strftime('%m-%d-%Y') + '_errorlog.txt'
-with open(file_name, 'w') as error_log:
+file_name = "ErrorLogs/" + datetime.now().strftime("%m-%d-%Y") + "_errorlog.txt"
+with open(file_name, "w") as error_log:
     if len(exceptions) > 0:
         json.dump(exceptions, fp=error_log, indent=4)
     else: 
-        error_log.write('no exceptions thrown')
+        error_log.write("no exceptions thrown")
